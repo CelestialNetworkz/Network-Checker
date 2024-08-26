de6a38cb1c991137c48b8a5a2e98a3d145d93d79
Pl4sma <148803510+pl4sma-arch@users.noreply.github.com>
2024-08-26 12:58:50 +0200

    Update LIFE CHECKER.py

--- LIFE CHECKER.py
+++ LIFE CHECKER.py
@@ -69,8 +69,7 @@ def home():
 
     
     def opt_4():
-        print(Colorate.Horizontal(Colors.red_to_yellow, '''    Made by ! 0xPrime [ 0xprimeisbackpt6 ] '''))
-        print(Colorate.Horizontal(Colors.red_to_yellow, '''    Check out Kyron Team : https://discord.gg/Rs4VnEpB '''))
+        print(Colorate.Horizontal(Colors.red_to_yellow, '''    Made by ! Plasma [ kfsman ] '''))
         input(Colorate.Horizontal(Colors.blue_to_cyan,'''    Press enter to Continue...'''))
         home()
 
